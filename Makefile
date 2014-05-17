35d68126b386c9b3931d6c86d697c0327d2647ac
Janice Yip <janiceyip@cs.dartmouth.edu>
2014-05-17 17:35:12 -0400

    creating socket

---
+++ Makefile
@@ -0,0 +1,39 @@
+# Filename: Lab6 Query Makefile
+# Description: The make file is to build up the query.
+CC = gcc
+CFLAGS = -Wall -pedantic -std=c11 -lcurl -ggdb
+SRCDIR = /src/
+UTILDIR = ../util/
+UTILFLAG = -ltseutil
+UTILLIB = $(UTILDIR)libtseutil.a 
+
+# my project details
+EXEC = AMStartup
+OBJS = src/AMStartup.o
+SRCS = src/AMStartup.c 
+HDRS = ../utils/src/amazing.h 
+
+
+
+#query:$(SRCS) $(HDRS) $(UTILLIB)
+#	$(CC) $(CFLAGS) -o $@ $(SRCS) -L$(UTILDIR) $(UTILFLAG)
+
+#$(UTILLIB): $(SRCS) $(HDRS)
+#	cd $(UTILDIR); make;
+
+AMStartup: $(SRCS) 
+	$(CC) $(CFLAGS) -o AMStartup $(SRCS)
+
+AMStartup.o: $(HDRS)
+	$(CC) $(CFLAGS) -c src/AMStartup.c 
+
+
+
+
+clean:
+	rm -f *~
+	rm -f *#
+	rm -f ./AMStartup
+	rm -f *.o
+	rm -f src/*.o
+#	cd $(UTILDIR); make clean;
--- src/AMStartup.c
+++ src/AMStartup.c
@@ -58,29 +58,33 @@
 
 // ---------------- System includes e.g., <stdio.h>
 #include <stdio.h>                           // printf
-#include <curl/curl.h>                       // curl functionality
 #include <sys/types.h> 
 #include <sys/stat.h> 
 #include <unistd.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 
 // ---------------- Local includes  e.g., "file.h"
-#include "amazing.h"
+#include "../util/src/amazing.h"
 
 
 // ---------------- Constant definitions
 
 // ---------------- Macro definitions
-
+#define SERV_PORT 3000 /*port*/
 
 // ---------------- Structures/Types
 
 // ---------------- Private variables
 
 // ---------------- Private prototypes
+int IsNotNumeric(char *input);
 
 
 /* =========================================================================== */
@@ -89,10 +93,93 @@
 
 int main(int argc, char* argv[])
 {
+	int nAvatars; 
+	int Difficulty; 
+	char *filename; 
+	int sockinit; 
+	struct sockaddr_in servaddr;
+
+	/******************************* args check *******************************/
+	if (argc < 4) {
+		fprintf(stderr, "Incorrect number of arguments. Exiting now.\n"); 
+		exit(1); 
+	}
+
+	// check the input for number of avatars 
+    if (IsNotNumeric(argv[1])) {
+		fprintf(stderr, "Number of avatars must be a number. Exiting now.\n"); 
+    	exit(1); 
+    } else if ((atoi(argv[1]) < 0)) {
+    	fprintf(stderr, "Number of avatars must be greater than 0. Exiting now.\n"); 
+    	exit(1); 
+    } else {
+    	nAvatars = atoi(argv[1]); 
+    	printf("Number avatars: %d\n", nAvatars); 
+    }
+
+    // check input for difficulty of maze 
+    if (IsNotNumeric(argv[2])) {
+		fprintf(stderr, "Difficulty must be a number. Exiting now.\n"); 
+    	exit(1); 
+    } else if ((atoi(argv[2]) < 0) || (atoi(argv[2]) > 9)) {
+    	fprintf(stderr, "Difficulty level must be between 0 and 9. Exiting now.\n"); 
+    	exit(1); 
+    } else {
+    	Difficulty = atoi(argv[2]); 
+    	printf("Difficulty %d\n", Difficulty); 
+    }
+
+    // check hostname 
+
+
+
+	/************************** send AM_INIT message **************************/
+	// create a socket for the client
+	if ((sockinit = socket (AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("Problem creating the socket.\n");
+		exit(2);
+	}
+
+	// creation of the socket
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr= inet_addr(argv[4]);
+	servaddr.sin_port =  htons(SERV_PORT); //convert to big-endian order
+
+	// connection of the client to the socket 
+	if (connect(sockinit, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+		perror("Problem connecting to the server.\n");
+		exit(3);
+	} else {
+		printf("connected\n"); 
+	}
+
+	// send the message
+	send(sockinit, sendline, strlen(sendline), 0);
+
+
+
+
+/*
+
+    AM_INIT
+
+AM_INIT	nAvatars	Difficulty
+AMStartup builds the AM_INIT message with the nAvatars and Difficulty and 
+sends it to the server on the AM_SERVER_PORT port identified in amazing.h.
+
+If the initialization succeeds, the server will respond with an AM_INIT_OK message.
+
+If nAvatars is greater than AM_MAX_AVATAR or the Difficulty is greater than 
+AM_MAX_DIFFICULTY, the server will respond with an AM_INIT_FAILED message.
+
+*/
+	/**************************** create log file ****************************/
+
 
 }
 
-
+/*
 
 The startup script, AMStartup, can be a shell script or C program. It should be able to handle these parameters at a minimum:
 
@@ -105,10 +192,13 @@ The startup script, AMStartup, can be a shell script or C program. It should be 
 Once AMStartup has validated the arguments, it will construct and send the AM_INIT message to the server. 
 When the server responds with AM_INIT_OK, AMStartup recovers the MazePort from that reply.
 
-Then AMStartup creates a new log file with the name Amazing_$USER_N_D.log, where $USER is the current userid, N is the value of nAvatars and D is the value of Difficulty. The first line of the file should contain $USER, the MazePort, and the date & time. For example, the following shell command would produce this first line:
+Then AMStartup creates a new log file with the name Amazing_$USER_N_D.log, where $USER is the current userid, 
+N is the value of nAvatars and D is the value of Difficulty. The first line of the file should contain $USER, 
+the MazePort, and the date & time. For example, the following shell command would produce this first line:
 
 echo $USER, 10829, `date` > $filename
-where $filename is a variable containing the name of the log file. Alternatively, if your AMStartup is a C program, then you may find the time() and ctime() functions in time.h helpful.
+where $filename is a variable containing the name of the log file. Alternatively, if your AMStartup is a C program, 
+then you may find the time() and ctime() functions in time.h helpful.
 Then AMStartup will start the N copies of the Avatar client (as threads or processes) with the parameters they will need:
 
 AvatarId (an integer generated by AMStartup, starting at 0 and incremented by one for each subsequent Avatar started)
@@ -123,3 +213,29 @@ While validity checking of the parameters in the startup script (program) is app
 Of course, if you would prefer to setup these parameters with "getopt" style switches, that's ok too.
 
 If you wish to add additional parameters, please speak with your customers (the TA and/or the instructor) first.
+
+
+*/
+
+
+
+/* =========================================================================== */
+/*                              IsNotNumeric	                               */
+/* =========================================================================== */
+/*  Checks that the input is a number
+ *  
+ *  @input: input from commandline (should be a number)
+ *
+ *  Returns 0 on success; otherwise, -1, if the inputed string is not a number.
+ */
+int IsNotNumeric(char *input)
+{
+    int i;
+    for ( i=0; i<strlen(input); i++ ) {
+        if (!isdigit(input[i])) {
+            fprintf(stderr, "Not a number.\n");
+            return(-1); // failure
+        }
+    }
+    return(0); // success
+}
