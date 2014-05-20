35b47f894afd3109af3236f63d38a2e885a8d133
Lotanna M. Ezenwa <lmezenw@cs.dartmouth.edu>
2014-05-19 22:12:54 -0400

    Added Tests for CompareFunction and updated DSL.h as well as Heuristic

--- src/dstarlite.c
+++ src/dstarlite.c
@@ -6,9 +6,17 @@
 
 
 
+
+
+
 int heuristic(XYPos *p1, XYPos *p2){
 	uint32_t dx = p1->x - p2->x;
 	uint32_t dy = p1->y - p2->y;
 	return abs(dx) + abs(dy);
+}
+
+
+int compareCosts(MazeNode *p1, MazeNode *p2){
+	return (p1->costToGoal < p2->costToGoal);
 }
 
