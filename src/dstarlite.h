58ce47587498ed68efee0c9f0fb234199ebc9d76
Janice Yip <janiceyip@cs.dartmouth.edu>
2014-05-20 13:19:12 -0400

    added header to dstarlite.h

--- src/dstarlite.h
+++ src/dstarlite.h
@@ -1,26 +1,43 @@
+/* ========================================================================== */
+/* File: dstarlite.h
+ *
+ * Project name: CS50 Amazing Project
+ * Component name: dstarlite header
+ *
+ * Primary Author: Oseleta
+ * Date Created: Mon May 20 2014
+ *
+ * This file contains the common defines for the dstarlite algorithm.
+ *
+ */
+/* ========================================================================== */
 #ifndef DSTARL_H
 #define DSTARL_H
 
+// ---------------- Prerequisites e.g., Requires "math.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include 
+
+// ---------------- Local includes  e.g., "file.h"
 #include "../util/src/amazing.h"
 #include "../util/src/queue.h"
 
-
+// ---------------- Structures/Types
 typedef enum {WALL, PATH} status;
 typedef enum {TRAVELED, UNVISITED} pathstatus;
 typedef enum {NEW, OPEN, CLOSED,RAISE, LOWER} mark;
 
-typedef struct NodeKey{
-	float val1 = INFINITY;
+typedef struct NodeKey
+{
+	float val1;
 	float val2;
-}NodeKey;
+} NodeKey;
 
 
 
-typedef struct MazeNode{
+typedef struct MazeNode
+{
 
 	NodeKey key;
 	float costToGoal;
@@ -32,35 +49,36 @@ typedef struct MazeNode{
 	pathstatus ps;
 	mark mk;
 	
+} MazeNode;
 
-}MazeNode;
 
-typedef struct Graph{
+typedef struct Graph
+{
+	MazeNode **table;	
+} Graph;
 
-	MazeNode **table;
-	
-}Graph;
-
-typedef struct PathNode{
-
-		XYPos position;
-		struct PathNode *next;
-		struct PathNode *prev;
+typedef struct PathNode
+{
+	XYPos position;
+	struct PathNode *next;
+	struct PathNode *prev;
 } PathNode;
 
-typedef struct OpenQueue{
+typedef struct OpenQueue
+{
 	PathNode *head;
 	PathNode *tail;
 } OpenQueue;
 
 
 
-typedef struct Path{
+typedef struct Path
+{
 	PathNode *head;
 	PathNode *tail;
-}Path;
+} Path;
 
-
+// ---------------- Private prototypes
 int MH(MazeNode n1, MazeNode n2);
 int heuristic(XYPos *p1, XYPos *p2);
 int compareCosts(void *p1,void *p2);
