29af353a3adc207674ab3652c91f642deeffaca7
Lotanna M. Ezenwa <lmezenw@cs.dartmouth.edu>
2014-05-28 21:11:02 -0400

    Fixed unit testing.c

--- util/testing/unit_testing.c
+++ util/testing/unit_testing.c
@@ -5,10 +5,7 @@
 
 static int TEST1(); //Parsing Log file
 static int TEST2(); //Freeing of Maze
-static int TEST3(); 
-static int TEST4(); 
-static int TEST5(); 
-static int TEST6(); 
+
 
 
 int main(int argc, char *argv[]){
@@ -17,9 +14,7 @@ int main(int argc, char *argv[]){
 	 
 	if(TEST1()){fprintf(stderr, "ERROR DURING TEST 1"); return 1;}
 	if(TEST2()){fprintf(stderr, "ERROR DURING TEST 2"); return 2;}
-	if(TEST3()){fprintf(stderr, "ERROR DURING TEST 3"); return 3;}
-	if(TEST4()){fprintf(stderr, "ERROR DURING TEST 4"); return 4;}
-	if(TEST5()){fprintf(stderr, "ERROR DURING TEST 5"); return 5;}
+	
 	
 	return 0;
 }
@@ -33,32 +28,13 @@ static int TEST1(){
 }
 
 static int TEST2(){
-	int b = 1;
 	MazeCell ***test_array;
 	test_array = parselog(25,25);
 	if (!test_array) return 1;
 	freeMaze(test_array,25,25); //Freeing function;
-	return b;
-}
-
-static int TEST3(){
-	int c = 1;
 	
-	
-	
-	
-	
-	return c;
-	
-
-}
-
-static int TEST4(){
 	return 0;
 }
 
-static int TEST5(){
-	return 0;
-}
 
 
